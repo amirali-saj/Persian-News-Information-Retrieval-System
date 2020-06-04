c25a87eb3b5b7f2eebc421a3fe461de02b2b99f2
Amirali <Amirali.sajjadi98>
2020-06-04 18:36:44 +0430

    add_posting_list_from_file method implemented.

--- util/file.py
+++ util/file.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+from indexing.inverted_index import InvertedIndex
 
 
 def read_text_file():
@@ -18,3 +19,21 @@ def fetch_csv_from_file(path):
 def fetch_docs_from_file(path):
     docs = fetch_csv_from_file(path)
     return docs[1:]
+
+
+def add_posting_list_from_file(word_id, inverted_index):
+    file_name = find_word_id_file_name(word_id)
+    posting_lists = fetch_csv_from_file(file_name)
+    for posting_list in posting_lists:
+        word_id = posting_list[0]
+        for i in range(len(posting_list)):
+            if i < 1:
+                continue
+            inverted_index.add_term_by_id(word_id, posting_list[i])
+    return inverted_index
+
+
+def find_word_id_file_name(word_id):
+    index = word_id % 2000
+    name = 'files/postings/postings' + str(index) + '-' + str((index + 2000)) + '.csv'
+    return name
