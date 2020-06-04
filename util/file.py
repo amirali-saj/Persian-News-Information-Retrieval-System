5b057daf05a08646980addacd447bbf45b0b1e15
Amirali <Amirali.sajjadi98>
2020-06-04 18:05:38 +0430

    Added fetch_csv_from_file method

--- util/file.py
+++ util/file.py
@@ -1,5 +1,20 @@
+import sys
+import csv
 
 
 def read_text_file():
-    file = open('files/stop_words.txt','r')
+    file = open('files/stop_words.txt', 'r')
     return file.read()
+
+
+def fetch_csv_from_file(path):
+    csv.field_size_limit(sys.maxsize)
+    with open(path, newline='') as f:
+        reader = csv.reader(f)
+        result = list(reader)
+    return result
+
+
+def fetch_docs_from_file(path):
+    docs = fetch_csv_from_file(path)
+    return docs[1:]
