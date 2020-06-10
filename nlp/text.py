7fd1cb03cb0222a5b2e2db40ea9d7f32cb88bf7d
Amirali <Amirali.sajjadi98>
2020-06-10 15:16:16 +0430

    normalize text function developed.

--- nlp/text.py
+++ nlp/text.py
@@ -17,3 +17,50 @@ def stop_word_filter(content, stop_words):
             if word not in stop_words:
                 result.append(word)
         return result
+
+
+normalization_table = {}
+
+normalization_list_table = [
+    (['ب', 'ﺒ', 'ﺐ', 'ﺑ'], 'ب'),
+    (['ء', 'ئ', 'ﻰ', 'ﯾ', 'ﯿ', 'ئ', 'ي'], 'ی'),
+    (['ك''ﮐ', 'ک'], 'ک'),
+    (['ﻨ', 'ﻦ', 'ﻧ'], 'ن'),
+    (['ﺻ'], 'ص'),
+    (['ﻀ'], 'ض'),
+    (['ﻘ'], 'ق'),
+    (['ﺶ', 'ﺸ', 'ﺷ'], 'ش'),
+    (['ﺛ'], 'ث'),
+    (['ف'], 'ف'),
+    (['ﮔ', 'ﮕ'], 'گ'),
+    (['ﻪ', 'ﻬ', 'ﻫ', 'ة'], 'ه'),
+    (['ﺖ', 'ﺘ', 'ﺗ'], 'ت'),
+    (['ﻤ', 'ﻣ'], 'م'),
+    (['ﺨ', 'ﺧ'], 'خ'),
+    (['ﺴ', 'ﺳ', 'ﺲ'], 'س'),
+    (['ﺣ'], 'ح'),
+    (['ﻌ'], 'ع'),
+    (['ﻮ', 'ﻓ', 'ؤ'], 'و'),
+    (['ﺪ'], 'د'),
+    (['ﺮ'], 'ر'),
+    (['إ', 'أ', 'آ'], 'ا'),
+    (['َ', '“', 'ّ,', 'ً', '؛', 'ُ', 'ٔ', '؟', ',', 'ِ', '”', '\u200f', '\u200e', '\u200d', '\u202c', '\u200a',
+      '\u202b'], ''),
+    (['\u1680', '\u2005', '\u2006', '\u2009', '\u200A', '\u200B', '\u202F', '\uFEFF'], '-')
+]
+
+for x in normalization_list_table:
+    print(x[0])
+    for char in x[0]:
+        normalization_table[char] = x[1]
+        print(char, '\n', x[1])
+        print('\n\n')
+
+
+def normalize(text):
+    new_text = ''
+    for c in text:
+        replacement = normalization_table.get(c, c)
+        new_text += replacement
+    return text
+
